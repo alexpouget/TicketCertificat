91735e09165045cdcfc95725906815da817f283a
Alex Pouget <pouget.al@gmail.com>
2016-05-01 12:28:40 +0200

    init project

---
+++ src/main/java/com/esgi/TicketCertificatApplication.java
@@ -0,0 +1,12 @@
+package com.esgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCertificatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCertificatApplication.class, args);
+	}
+}
